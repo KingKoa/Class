298cabce5061aaeb10324e8659877677a822d84c
Kekoapoaono Montalbo <kekoapoaono@icloud.com>
2019-05-07 23:57:36 -0600

    Finisheddd

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-/*window.onload = function(){
-    document.getElementsByClassName('logo').addEventListener('click', function(){
-        document.getElementsByClassName('logo').innerHTML = "BlahBlahBlah";
+window.onload = function(){
+    var imageNum = 0;
+    document.querySelector("button").addEventListener("click", 
+        function(){
+            ++imageNum;
+            if (imageNum > 7)
+            {
+                imageNum = 0;
+            }
+
+            if (imageNum == 0){
+                document.querySelector("header").style.backgroundImage = "url('images/ocean.jpg')";
+            }
+            if (imageNum == 1){
+                document.querySelector("header").style.backgroundImage = "url('images/Clouds.jpg')";
+            }
+            if (imageNum == 2){
+                document.querySelector("header").style.backgroundImage = "url('images/ConnectingStars.jpg')";
+            }
+
+            if (imageNum == 3){
+                document.querySelector("header").style.backgroundImage = "url('images/LightBulb.jpg')";
+            }
+
+            if (imageNum == 4){
+                document.querySelector("header").style.backgroundImage = "url('images/Mountain.jpg')";
+            }
+
+            if (imageNum == 5){
+                document.querySelector("header").style.backgroundImage = "url('images/OldCastle.jpeg')";
+            }
+
+            if (imageNum == 6){
+                document.querySelector("header").style.backgroundImage = "url('images/Outerspace.jpeg')";
+            }
+
+            if (imageNum == 7){
+                document.querySelector("header").style.backgroundImage = "url('images/SunSetCastle.jpeg')";
+            }
     });
 }
-
-
-function clickedButton(){
-    //document.getElementsByClassName("logo")
-    div {
-        background: rgb(0, 0, 0);
-    }
-}
-*/
