eb4a36eca4e30b36e3fced4b4359dde26f8f1212
Kekoapoaono Montalbo <kekoapoaono@icloud.com>
2019-05-07 19:09:15 -0600

    I just reworked the page and it's layout and some images

---
+++ script.js
@@ -0,0 +1,14 @@
+/*window.onload = function(){
+    document.getElementsByClassName('logo').addEventListener('click', function(){
+        document.getElementsByClassName('logo').innerHTML = "BlahBlahBlah";
+    });
+}
+
+
+function clickedButton(){
+    //document.getElementsByClassName("logo")
+    div {
+        background: rgb(0, 0, 0);
+    }
+}
+*/
